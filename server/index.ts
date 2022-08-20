7e5d79c9aaeaeb254078d6f8d53ad03307c4fec9
Omar Khan <khancodegt@gmail.com>
2022-08-20 14:16:00 -0400

    install typescript dependencies

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express, { Express, Request, Response } from 'express'
+const app: Express = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`⚡️ Server is running at https://localhost:${port}`)
+})
