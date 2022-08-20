29437434bd29f9b72447111a6e30a2d6142500d4
Omar Khan <khancodegt@gmail.com>
2022-08-20 14:50:45 -0400

    add concurrently & nodemon to improve developer experience

---
+++ server/README.md
@@ -0,0 +1,18 @@
+# amm-dex-xrpl-server
+A REST API server that interacts with rippled and MongoDB for serving XRPL AMM requests/transactions.
+
+# Installation
+```
+$ npm i
+```
+
+# Usage
+### Production:
+```
+$ npm start
+```
+
+### Development:
+```
+$ npm run dev
+```
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from 'express'
+import express from 'express'
+import type { Express, Request, Response } from 'express'
 const app: Express = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('TypeScript + Express server!')
 })
 
 app.listen(port, () => {
