f097dbd867f2f3e945eb058ba16bd07a687084b9
Omar Khan <khancodegt@gmail.com>
2022-08-22 19:03:04 -0400

    install body-parser

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,22 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 import type { Express, Request, Response } from 'express'
 const app: Express = express()
 const port = 3000
 
+// parse application/x-www-form-urlencoded - the default body type for forms
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json - the JSON payload sent by a REST client
+app.use(bodyParser.json())
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('TypeScript + Express server!')
+})
+
+app.post('/amm', (req: Request, res: Response) => {
+  res.send('POST /amm success')
+  console.log(req.body)
 })
 
 app.listen(port, () => {
